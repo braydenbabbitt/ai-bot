3ccb434caf1022c4990a5d9ecf916074ac7f982b
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-12-26 22:12:10 -0700

    init gemini interaction

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if (api_key == None):
+    raise RuntimeError("No API key found")
+
+from google import genai
+
+
 def main():
-    print("Hello from ai-bot!")
+    client = genai.Client(api_key=api_key)
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    res = client.models.generate_content(model="gemini-2.5-flash", contents=user_prompt)
+    if (res.usage_metadata == None):
+        raise RuntimeError("No metadata")
+    print("User prompt: " + user_prompt)
+    print("Prompt tokens: " + str(res.usage_metadata.prompt_token_count))
+    print("Response tokens: " + str(res.usage_metadata.candidates_token_count))
+    print("Response: " + res.text)
 
 
 if __name__ == "__main__":
